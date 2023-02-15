0d323b1814aff4ac2c0c5e359a58eb0b30955bdd
Peter Kehl <peter.kehl@gmail.com>
2023-02-15 15:47:10 -0800

    Factored out to a top level function

--- src/lib.rs
+++ src/lib.rs
@@ -7,26 +7,22 @@ macro_rules! num_slots {
     };
 }
 
-pub struct S {}
+pub fn f() {
+    #[allow(dead_code)]
+    #[allow(unused_braces)]
+    struct Inner<const C: usize>
+    where
+        [(); { num_slots!(C) }]:, {
+        /* An asterisk-based comment here is required to trigger fmt bug.
 
-impl S {
-    pub fn f() {
-        #[allow(dead_code)]
-        #[allow(unused_braces)]
-        struct Inner<const C: usize>
-        where
-            [(); { num_slots!(C) }]:, {
-            /* An asterisk-based comment here is required to trigger fmt bug.
+        A single-line double-slash-prefixed comment is not enough. A
+        single-line double-slash-prefixed comment is not enough either.
+        Side note: If you have a combination of two, or all three of the
+        above mentioned types of comments here, some of they disappear
+        after `cargo fmt`.
 
-            A single-line double-slash-prefixed comment is not enough. A
-            single-line double-slash-prefixed comment is not enough either.
-            Side note: If you have a combination of two, or all three of the
-            above mentioned types of comments here, some of they disappear
-            after `cargo fmt`.
-
-            The above comment applied with a field definition following this
-            comment, too.
-            */
-        }
+        The above comment applied with a field definition following this
+        comment, too.
+        */
     }
 }
