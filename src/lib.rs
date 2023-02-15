05632fba2c163742f3976e11d5661a78e9486400
Peter Kehl <peter.kehl@gmail.com>
2023-02-15 15:46:11 -0800

    Factored out of a module to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,32 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 
-pub mod vec;
+macro_rules! num_slots {
+    ($c:expr) => {
+        $c + 1
+    };
+}
+
+pub struct S {}
+
+impl S {
+    pub fn f() {
+        #[allow(dead_code)]
+        #[allow(unused_braces)]
+        struct Inner<const C: usize>
+        where
+            [(); { num_slots!(C) }]:, {
+            /* An asterisk-based comment here is required to trigger fmt bug.
+
+            A single-line double-slash-prefixed comment is not enough. A
+            single-line double-slash-prefixed comment is not enough either.
+            Side note: If you have a combination of two, or all three of the
+            above mentioned types of comments here, some of they disappear
+            after `cargo fmt`.
+
+            The above comment applied with a field definition following this
+            comment, too.
+            */
+        }
+    }
+}
