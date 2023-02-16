fd0f58c9eef0936729df9ac2f19a049bb3204bdd
Peter Kehl <peter.kehl@gmail.com>
2023-02-15 16:41:38 -0800

    Factored out of an outer function

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,25 @@
 
 macro_rules! num_slots {
     ($c:expr) => {
-        $c + 1
+        1
     };
 }
 
-pub fn f() {
-    #[allow(dead_code)]
-    #[allow(unused_braces)]
-    struct Inner<const C: usize>
-    where
-        [(); { num_slots!(C) }]:, {
-        /* An asterisk-based, or a double-slash-prefixed, comment here is
-           required to trigger the fmt bug.
+#[allow(dead_code)]
+#[allow(unused_braces)]
+struct Inner<const C: usize>
+where
+    [(); { num_slots!(C) }]:, {
+    /* An asterisk-based, or a double-slash-prefixed, comment here is
+       required to trigger the fmt bug.
 
-        A single-line triple-slash-prefixed comment (with a field following it) is not enough - it will not trigger the fmt bug.
+    A single-line triple-slash-prefixed comment (with a field following it) is not enough - it will not trigger the fmt bug.
 
-        Side note: If you have a combination of two, or all three of the
-        above mentioned types of comments here, some of them disappear
-        after `cargo fmt`.
+    Side note: If you have a combination of two, or all three of the
+    above mentioned types of comments here, some of them disappear
+    after `cargo fmt`.
 
-        The bug gets triggered even if a field definition following the
-        (asterisk-based, or a double-slash-prefixed) comment, too.
-        */
-    }
+    The bug gets triggered even if a field definition following the
+    (asterisk-based, or a double-slash-prefixed) comment, too.
+    */
 }
