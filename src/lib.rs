adda6105d8e22b652570983865f95d7537442760
Peter Kehl <peter.kehl@gmail.com>
2023-02-15 16:04:19 -0800

    Comments

--- src/lib.rs
+++ src/lib.rs
@@ -13,16 +13,17 @@ pub fn f() {
     struct Inner<const C: usize>
     where
         [(); { num_slots!(C) }]:, {
-        /* An asterisk-based comment here is required to trigger the fmt bug.
+        /* An asterisk-based, or a double-slash-prefixed, comment here is
+           required to trigger the fmt bug.
 
-        A single-line double-slash-prefixed comment is not enough. A
-        single-line double-slash-prefixed comment is not enough either.
+        A single-line triple-slash-prefixed comment (with a field following it) is not enough - it will not trigger the fmt bug.
+
         Side note: If you have a combination of two, or all three of the
         above mentioned types of comments here, some of them disappear
         after `cargo fmt`.
 
-        The above comment applied with a field definition following this
-        comment, too.
+        The bug gets triggered even if a field definition following the
+        (asterisk-based, or a double-slash-prefixed) comment, too.
         */
     }
 }
